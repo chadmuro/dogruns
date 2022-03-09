748bae90b872cf244e394ed22be756665bf0d29e
chadmuro <chadmuro@gmail.com>
2022-03-09 12:42:25 +0900

    install tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,47 +8,23 @@ const Header: React.FC = () => {
     router.pathname === pathname;
 
   let left = (
-    <div className="left">
+    <div>
       <Link href="/">
-        <a className="bold" data-active={isActive("/")}>
-          Feed
-        </a>
+        <a data-active={isActive("/")}>Dog Runs</a>
       </Link>
-      <style jsx>{`
-        .bold {
-          font-weight: bold;
-        }
-
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
-
-        .left a[data-active="true"] {
-          color: gray;
-        }
-
-        a + a {
-          margin-left: 1rem;
-        }
-      `}</style>
     </div>
   );
 
-  let right = null;
+  let right = (
+    <div>
+      <button>Login</button>
+    </div>
+  );
 
   return (
-    <nav>
+    <nav className="flex p-2 items-center justify-between">
       {left}
       {right}
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-      `}</style>
     </nav>
   );
 };
