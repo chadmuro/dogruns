b8ae87babeb2c93ddbda7fd2a27d9c08abc34771
chadmuro <chadmuro@gmail.com>
2022-03-28 11:06:59 +0900

    add ui for park review form

---
+++ components/forms/Review/index.tsx
@@ -0,0 +1,69 @@
+import { UseFormRegister } from "react-hook-form";
+import Button from "../../shared/Button";
+import StarRating from "./StarRating";
+
+export type ReviewFormInputs = {
+  comment: string;
+};
+
+interface ReviewFormProps {
+  rating: number;
+  handleValueChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<ReviewFormInputs>;
+  onSubmit: () => void;
+}
+
+const ReviewForm = ({
+  rating,
+  handleValueChange,
+  register,
+  onSubmit,
+}: ReviewFormProps) => {
+  return (
+    <form className="mb-8" onSubmit={onSubmit}>
+      <label
+        htmlFor="comment"
+        className="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-400"
+      >
+        Your Review
+      </label>
+      <div className="flex mb-2">
+        <StarRating
+          value={1}
+          handleValueChange={handleValueChange}
+          rating={rating}
+        />
+        <StarRating
+          value={2}
+          handleValueChange={handleValueChange}
+          rating={rating}
+        />
+        <StarRating
+          value={3}
+          handleValueChange={handleValueChange}
+          rating={rating}
+        />
+        <StarRating
+          value={4}
+          handleValueChange={handleValueChange}
+          rating={rating}
+        />
+        <StarRating
+          value={5}
+          handleValueChange={handleValueChange}
+          rating={rating}
+        />
+      </div>
+      <textarea
+        {...register("comment")}
+        id="comment"
+        rows={4}
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
+        placeholder="Leave a comment..."
+      />
+      <Button type="submit" text="Submit a review" />
+    </form>
+  );
+};
+
+export default ReviewForm;
--- components/park/Reviews.tsx
+++ components/park/Reviews.tsx
@@ -1,10 +1,32 @@
-import Button from "../shared/Button";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import ReviewForm, { ReviewFormInputs } from "../forms/Review";
 
 const Reviews = () => {
+  const [rating, setRating] = useState(1);
+  const { register, handleSubmit } = useForm<ReviewFormInputs>({
+    defaultValues: {
+      comment: "",
+    },
+  });
+
+  function handleValueChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setRating(parseInt(event.target.value, 10));
+  }
+
+  const onSubmit: SubmitHandler<ReviewFormInputs> = (data) => {
+    console.log(data, rating);
+  };
+
   return (
     <section>
-      <h2 className="text-xl">Reviews</h2>
-      <Button type="button" text="Write a review" />
+      <h2 className="text-xl mb-4">Reviews</h2>
+      <ReviewForm
+        rating={rating}
+        handleValueChange={handleValueChange}
+        register={register}
+        onSubmit={handleSubmit(onSubmit)}
+      />
     </section>
   );
 };
